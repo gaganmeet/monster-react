dce0d0758f578a143e56a97af6f1b6f2e966649b
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2020-09-29 09:06:54 +0530

    Updates

---
+++ precache-manifest.7711c59907bb42cd1debf5a613c069cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5357b6663d20b661622c4914bd3019b4",
+    "url": "/monster-react/index.html"
+  },
+  {
+    "revision": "b42e391bc231b3aa02c3",
+    "url": "/monster-react/static/css/main.7e7d9569.chunk.css"
+  },
+  {
+    "revision": "572d27cb9fb9f4d9ed2a",
+    "url": "/monster-react/static/js/2.0ca3f07a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-react/static/js/2.0ca3f07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42e391bc231b3aa02c3",
+    "url": "/monster-react/static/js/main.33e07419.chunk.js"
+  },
+  {
+    "revision": "7ce95747e08feddb8707",
+    "url": "/monster-react/static/js/runtime-main.d99b2268.js"
+  }
+]);
